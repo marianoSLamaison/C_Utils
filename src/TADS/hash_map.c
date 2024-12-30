75cf46a03ece824c8a12a60a1eeda737ce6bdec4
Mariano Sebastian <mlamaison@frba.utn.edu.ar>
2024-12-29 23:49:20 -0300

    Mañana hago las pruebas

--- src/TADS/hash_map.c
+++ src/TADS/hash_map.c
@@ -1,74 +1,256 @@
 #include "hash_map.h"
 
+#define DEFAULT_MAP_SIZE 0xFF
+#define MAX_INDEX_NODES 0x1F
+//why this way? because why not?
+//the only thing bothering me was conmutatibility onestly
+//
+//My tree rules for a hash func
+//1. No conmutatibility, it bothers me.
+//2. No default values (if I had gone with the "treat them as vectors and cross produc them" I will have had
+//   A lot of stuff just defaulting to 0.
+//3. No calling functions. soo nothing that can't be calculated right here.
+//   (macros are fine, as long as they don't call functions in secret)
+#define g(n) (((n-n%0x08)<<0x0C)*3 + n*n)
+#define REHASH_NEEDED 52
+
+struct INDEX_NODE
+{
+	unsigned char key;
+	size_t key_size;
+	void * val;
+	struct NODO * next;
+};
+
+struct INDEX_LIST
+{
+	struct NODO* head;
+	int count;//el cap es igual para todos, 
+		  //a si que lo dejo como una constnte global
+};
+
+typedef struct HASH_MAP
+{
+	int cap;
+	struct INDEX_LIST * indices;
+}t_hmap;
+
 t_hmap* hmap_create(void)
 {
 	t_hmap* ret = malloc(sizeof(t_hmap));
-	//128 elementos de base deberia ser suficiente, calculo.
-	ret->cap = 0xEF;//todos los hmaps tienen minimo 16 espaios libres
-	ret->elem_c = 0;
-	ret->values = malloc(sizeof(t_hmapi) * ret->cap);
-	int i,j;
-	t_index* indice;
-	for(i=0; i<ret->cap; i++)
+	struct INDEX_LIST* ret_indices;
+	int i;
+	ret->cap = DEFAULT_MAP_SIZE;
+	ret_indices = malloc(sizeof(struct INDEX_LIST) * DEFUALT_MAP_SIZE);
+	for(i=0; i<DEFAULT_MAP_SIZE; i++)
 	{
-		indice = &ret->indices[i];
-		indice->cap = 0x10;
-		indice->bussy_indexes = 0;
-		ret->indices = malloc(sizeof(t_sindex)*0x10);
-		for ( j=0; j<0x10; j++) 
-		{
-			ret->indices[i].key=NULL;
-			ret->indices[i].val == NULL;
-		}
+		(ret + i)->head = NULL;
+		(ret + i)->count = 0;
 	}
+	ret->indices = ret_indeices;
 	return ret;
 }
-//why this way? because why not?
-//the only thing bothering me was conmutatibility onestly
-#define g(n) (((n-n%0x08)<<0x0C)*3 + n*n)
 
-static unsigned long hash_func(void* _key, int k_length)
+static int get_index_from_key(unsigned char* key, size_t key_size, int cap)
 {
-	unsigned char* key = (signed char*)_key;
-	unsigned char n;
 	unsigned long hash = 0;
 	int i;
 	
-	for(i=0; i<k_length; i++)
+	for(i=0; i<key_size; i++)
 	{
-		hash += 1<<(n & 0x07);//Fuck conmutatibility, all my homies hate 
+		//creo que asi deberia quitar la conmutatibilidad
+		hash += 1<<(hash & 0x07);//Fuck conmutatibility, all my homies hate 
 		//conmutatibility
 		n = key[i];
 		hash += g(n);
 	}
-	return hash;
+	return hash % cap;
 }
-
-static int get_index_from_key(signed char* key, t_hmap* map)
+static int index_list_push(struct INDEX_LIST* list, unsigned char key, size_t key_size, void* val)
 {
-	long ret;
-	int num_cs = strlen(key) - 1;
-	unsigned long hash = hash_func(key,num_cs);
-	return hash % map->elem_c;
-}
+	struct INDEX_NODE* new_node = malloc(sizeof(struct INDEX_NODE));
+	if (new_node == NULL) return FAILURE_CREATING;
+	new_node->key      = key;
+	new_node->key_size = key_size;
+	new_node->val      = val;
 
-void hmap_add(signed char* key, void* val, t_hmap* map)
-{
-	//no voy a ponerme a rehashear eternamente por un "tal vez" de mejor eficiencia
-	//si hay mas de un item, toca chequear linialmente para eso confiamos en que la funcion
-	//tenga una chance baja de dar repetidos
-	t_index* indice = map->indices[get_index_from_key(key, map)];
-	if ( index->elem_c == 0 )
+	if (list->head == NULL)
 	{
-		index->indices[0].key = key;
-		index->indices[0].val = val;
-		index->elem_c=1;
-		//incrementamos el numeor de indices ocupados
-		//si llega a cierta marca
-		map->bussy_indexes++;
-		return;
+		new_node->next = NULL;
+		list->head = new_node;
+		goto FINISH;
 	}
-	if ( index->elem_c < 
+	new_node->next = list->head;
+	list->head = new_node;
+FINISH:
+	list->count++;//pegamos el valor de todas formas a si no tenemos que hacer mas cosas luego
+	if (list->count > MAX_INDEX_NODES) return REHASH_NEEDED; 
+	return 0;
+}
+static int index_list_push_node(struct INDEX_LIST* list, struct INDEX_NODE* nodo)
+{
+	if (list->head==NULL)
+	{
+		list->head = nodo;
+		list->count++;
+		return 0;
+	}
+	nodo->next = list->head;
+	list->head = nodo;
+	list->count++;
+	if (list->count > MAX_INDEX_NODES) return REHASH_NEEDED;
+	return 0;
+}
+
+static struct INDEX_NODE* index_list_pop(struct INDEX_LIST* list)
+{
+	struct INDEX_NODE* ret = list->head;
+	list->head = list->head->next;
+	list->count--;
+	return ret;
+}
+
+static int hmap_rehash(t_hmap* map)
+{
+	struct INDEX_LIST* new_ilist = malloc(sizeof(struct INDEX_LIST) * map->cap * 2);
+	int i, new_cap = map->cap*2, indice;
+	struct INDEX_NODE* nodo;
+	if (new_ilist == NULL) return FAILURE_REHASHING;
+	for(i=0; i<new_cap; i++)
+	{
+		(new_ilist + i)->head = NULL;
+		(new_ilist + i)->count = 0;
+	}
+	for(i=0; i<map->cap; i++)
+	{
+		while((map->indices + i)->head != NULL)
+		{
+			nodo = index_list_pop(map->indices + i);
+			indice = get_index_from_key(nodo->key, nodo->key_size, new_cap);
+			index_list_push_node(new_ilist + indice, nodo);
+		}
+	}
+	free(map->indices);
+	map->indices = new_ilist;
+	return 0;
+}
+
+static int cmp_keys(unsigned char* key1, size_t key_size1, unsigned char key2, size_t key_size2)
+{
+	int i;
+	if (key1 == key2) return 0;//si apuntan a lo mismo son lo mismo en lo que a mi respecta
+	if (key_size1 < key_size2) return -1;
+	if (key_size1 > key_size2) return  1;
+
+	for(i=0; i<key_size1; i++)
+	{
+		if (*(key1+i) < *(key2+i)) return -1;
+		if (*(key1+i) > *(key2+i)) return  1;
+	}
+	return 0;
+}
+
+static struct INDEX_NODE* list_get_node(struct INDEX_LIST* list, unsigned char key, size_t key_size)
+{
+	struct INDEX_NODE* nodo = list->head;
+	while(nodo != NULL)
+	{
+		if (cmp_keys(key, key_size, nodo->key, nodo->key_size) == 0) break;
+		nodo = nodo->next;
+	}
+	return nodo;
+}
+
+static struct INDEX_NODE* list_remove(struct INDEX_LIST* list, unsigned char* key, size_t key_size)
+{
+	struct INDEX_NODE* nodo = list->head;
+	struct INDEX_NODE* old_nodo = NULL;
 	
-	
+	while(nodo != NULL)
+	{
+		if (cmp_keys(key, key_size, nodo->key, nodo->size) == 0) 
+		{
+			if (old_nodo != NULL) 
+			{
+				old_nodo->next = nodo->next;
+				nodo->next = NULL;
+				break;
+			}
+			list->head = nodo->next;
+			nodo->next = NULL;
+			break;
+		}
+		old_nodo = nodo;
+		nodo = nodo->next;
+	}
+	return nodo;
+}
+
+void* hmap_remove_with_key(unsigned char* key, size_t key_size, t_hmap* map)
+{
+	int index = get_indes_from_key(key, key_size, map->cap);
+	struct INDEX_NODE* node = list_remove(map->indices + index, key, key_size);
+	void* val = node->val;
+	free(node->key);
+	free(node);
+	return val;
+}
+
+void* hmap_get_value(unsigned char* key, size_t key_size, t_hmap* map)
+{
+	int index = get_index_from_key(key, key_size, map->cap);
+	struct INDEX_NODE* node = list_get_node(map->indices + index, key, key_size);
+	return node->val;
+}
+
+int hmap_add(unsigned char* key, size_t key_size, void* val, t_hmap* map)
+{
+	int index = get_index_from_key(key, key_size, map->cap);
+	int ret_msg = EXITO;
+	int push_res = index_list_push(map->indices + index, key, key_size, val);
+	int rehash_res;
+	if (push_res == FAILURE_CREATING) ret_msg |= push_res;
+	if (push_res == REHASH_NEEDED) rehash_res = hmap_rehash(map);
+	if (rehash_res == FAILURE_REHASHING) ret_msg |= rehash_res;
+	return ret_msg;
+}
+
+void hmap_simple_destroy(t_hmap* map)
+{
+	int i;
+	struct INDEX_NODE* nodo;
+	struct INDEX_LIST* list;
+	for(i=0; i<map->cap; i++)
+	{
+		list = (map->indices + i);
+		while(list->head != NULL)
+		{
+			nodo = list_pop(map->indices + i);
+			free(nodo->key);
+			free(nodo->val);
+			free(nodo);
+		}
+	}
+	free(map->indices);
+	free(map);
+}
+
+void hmap_destroy_vals(void (*val_destroyer)(void*), t_hmap* map)
+{
+	int i;
+	struct INDEX_NODE* nodo;
+	struct INDEX_LIST* list;
+	for(i=0; i<map->cap; i++)
+	{
+		list = (map->indices + i);
+		while(list->head != NULL)
+		{
+			nodo = list_pop(map->indices + i);
+			free(nodo->key);
+			val_destroyer(nodo->val);
+			free(nodo);
+		}
+	}
+	free(map->indices);
+	free(map);
 }
--- src/TADS/hash_map.h
+++ src/TADS/hash_map.h
@@ -1,20 +1,60 @@
 #ifndef HASH_MAP_H
 #define HASH_MAP_H
-typedef struct H_MAP_SUB_INDEX
-{
-	unsigned char* key;
-	void* val;
-}t_sindex;
 
-typedef struct H_MAP_INDEX
-{
-	t_sindex* indices;
-	int elem_c, cap;
-}t_index;
+
 //NOTA: quiero que sea de referencia cerrada me parece mas justo
-typedef struct HASH_MAP
-{
-	int cap, bussy_indexes;
-	t_index * indices;
-}t_hmap;
+//por ahora solo hay dos mensajes de error
+//uno para si solamente no pudo generar
+//la entrada en el diccionario
+//y otro para si pudo generarla, necesito
+//hacer un rehash y no pudo.
+enum MENSAJES{
+	FAILURE_CREATING = 0x0F,
+	FAILURE_REHASHING = 0xF0,
+	EXITO = 0x00
+};
+//la definicion esta en el punto c
+//para que no anden tocando valores
+//que no deberian
+typedef struct HASH_MAP t_hmap;
+
+//crea un hash_map con valores default,
+//vacio
+t_hmap* hmap_create(void);
+/**
+ * Remueve un elemento del hash_map
+ * Retornra el elemento o NULL si no 
+ * encontro nada
+ * */
+void* hmap_remove_with_key(unsigned char* key, size_t key_size, t_hmap* map);
+/**
+ * Retorna el valor asociado a una key,
+ * sin removerlo del map en si.
+ * Retorna NULL si no encontro nada
+ * */
+void* hmap_get_value(unsigned char* key, size_t key_size, t_hmap* map);
+/**
+ * Añade un elemento al diccionario,
+ * Retorna 0 si la operacion fue exitosa
+ * Retorna un mensaje de error si no
+ * y no añade el elemento.
+ * Puede retornar los dos mensajes a la vez,
+ * para confirma use los mensajes a modo
+ * de mascara y compare
+ * EJ: ret & FAILURE_CREATING == FAILURE_CREATING
+ * */
+int hmanp_add(unsigned char* key, size_t key_size, void* val, t_hmap* map);
+/**
+ * Destruye todo el diccionario suponiendo
+ * que los vals* no tiene punteros a nada
+ * dentro sullo
+ * */
+void hmap_simple_destroy(t_hmap* map);
+/**
+ * Destruye todo el diccionario, pidiendo una
+ * funcion especial para destruir los valores
+ * en caso de que estos tengan punteros a 
+ * datos
+ * */
+void hmap_destroy_mv(void (*val_destroyer)(void*), t_hmap* map);
 #endif
